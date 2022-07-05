dfb0123a35538a355f3d0bb509ef1cdeaeb46728
McvCar <mcv_cpu@live.com>
2022-07-05 17:08:13 +0800

    添加tinyPng在线压缩模式

--- src/assets-menu.js
+++ src/assets-menu.js
@@ -1,8 +1,9 @@
 let Path 	= require('path')
 let Fs 		= require('fs');
-let imageminApi = require('../lib/imagemin.min')
-let packageCfg 	= require('../package.json')
+let imageminApi = require('../lib/imageminApi')
+let tinypngApi 	= require('../lib/tinypngApi')
 let statistical = require('./tools/statistical') 
+let packageCfg 	= require('../package.json')
 
 let AssetsMenu = {
 
@@ -91,76 +92,14 @@ let AssetsMenu = {
 			arrList.push(fileInfo.file);
 		}
 
-		this.onCompressPicture(arrList,imageFileList);
-	},
-
-	async getZipRate(){
-		let rate = parseInt(await Editor.Profile.getConfig(packageCfg.name,'zipRate')) || 30;
-		if(rate<0){
-			rate = 1;
-		}else if(rate>100){
-			rate = 100
+		let mode = await Editor.Profile.getConfig(packageCfg.name,'zipMode') || 0;
+		if(mode == 0){
+			console.log("---------------------压缩模式:Imagemin---------------------------");
+			imageminApi.compressPicture(arrList,imageFileList);
+		}else{
+			console.log("---------------------压缩模式:TinyPng---------------------------");
+			tinypngApi.compressPicture(arrList,imageFileList);
 		}
-
-		return rate;
-	},
-
-	/**
-	 * 调用压缩工具api
-	 * @param {Array<{file:string,uuid:string}>} fileInfo 
-	 */
-	 async onCompressPicture(arrList,imageFileList){
-		let rate = await this.getZipRate();
-		let pngRate = rate*0.01;
-		
-		console.log("---------------------正在压缩图片---------------------------");
-		console.log("压缩值:",rate+"%");
-
-        imageminApi.imagemin(arrList, {
-            plugins: [
-                imageminApi.imageminMozjpeg({ quality: rate  }), //压缩质量（0,1）
-                imageminApi.imageminPngquant({
-                    quality: [pngRate, Math.min(pngRate+0.25,1)]  //压缩质量（0,1）
-                })
-            ]
-			
-        }).then((arrRes) => {
-            console.log("压缩成功,详情:\n")
-			for (let i = 0; i < arrRes.length; i++) {
-				const res = arrRes[i];
-				this.onCompressedSucceed(imageFileList,res)
-			}
-
-        }).catch(err => {
-            console.log("压缩失败:",err)
-        });
-	},
-	
-	/**
-	 * 压缩成功
-	 * @param {Array<{file:string,uuid:string,size:number}>} imageFileList 
-	 * @param {Object<{data:Buffer,sourcePath:string}>} res 
-	 */
-	onCompressedSucceed(imageFileList,res){
-		let desc = ""
-		for (let i = 0; i < imageFileList.length; i++) {
-			const fileInfo = imageFileList[i];
-			if(fileInfo.file.replace(/\\/g,'/') == res.sourcePath){
-				const fileName = Path.basename(fileInfo.file);
-				const newSize = res.data.byteLength;
-				const rate = (fileInfo.size-newSize)/fileInfo.size;
-				const oldMb = (fileInfo.size / 1024).toFixed(1) + " KB";
-				const newMb = (newSize / 1024).toFixed(1) + " KB";
-				if(newSize < fileInfo.size){
-					Fs.writeFileSync(res.sourcePath, res.data)
-					desc += `${fileName}、压缩率:${parseInt(rate*100)}%、压缩前后大小:${newMb} / ${oldMb}`
-				}else{
-					desc += `${fileName}、无法继续压缩`
-				}
-				break;
-			}
-		}
-		console.log(desc);
 	},
 }
 
--- src/setting-panel.js
+++ src/setting-panel.js
@@ -4,6 +4,11 @@ let packageCfg = require('../package.json')
 Editor.Panel.define = Editor.Panel.define || ((args)=>{return args})
 
 
+function saveConfig(doms){
+    Editor.Profile.setConfig(packageCfg.name,'zipRate',doms.zipRateSlider.value);
+    Editor.Profile.setConfig(packageCfg.name,'zipMode',doms.zipModeTab.value);
+}
+
 module.exports = Editor.Panel.define({
     template: fs.readFileSync(path.join(__dirname, '../template/setting.html'), 'utf-8'),
     style: fs.readFileSync(path.join(__dirname, '../template/setting.css'), 'utf-8'),
@@ -11,19 +16,22 @@ module.exports = Editor.Panel.define({
     $: {
         zipRateSlider: '#zipRateSlider',
         saveBtn: '#saveBtn',
+        zipModeTab: '#zipModeTab',
     },
 
     async ready() {
-
         this.$.zipRateSlider.value = parseInt(await Editor.Profile.getConfig(packageCfg.name,'zipRate')) || 30;
+        this.$.zipModeTab.value = parseInt(await Editor.Profile.getConfig(packageCfg.name,'zipMode')) || 0;
+        this.$.zipModeTab.addEventListener('click',()=>{
+            this.$.zipModeTab.value == 1 ? this.$.zipRateSlider.disabled = true : this.$.zipRateSlider.removeAttribute('disabled');
+            saveConfig(this.$);
+        },0)
+
         this.$.saveBtn.addEventListener('click',()=>{
-            this.saveConfig();
+            saveConfig(this.$);
         },0)
     },
 
-    saveConfig(){
-        Editor.Profile.setConfig(packageCfg.name,'zipRate',this.$.zipRateSlider.value);
-    },
 
     listeners: {
     },
@@ -31,6 +39,6 @@ module.exports = Editor.Panel.define({
     },
     beforeClose() { },
     close() { 
-        this.saveConfig();
+        saveConfig(this.$);
     },
 });
